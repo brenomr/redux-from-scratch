f997ae5a0fb99bc169e3e7ca0d726ebd43358ab0
Breno Rodrigues <brenocartoon@hotmail.com>
2021-09-29 23:45:37 -0300

    Update index for tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+import store from './store';
+import * as actions from './actions';
+
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
+
+store.dispatch(
+  actions.bugAdded("Bug numero 001")
+);
+
+store.dispatch(
+  actions.bugAdded("Bug do agente 007")
+);
+
+store.dispatch(
+  actions.bugResolved(1, true)
+)
+
+unsubscribe();
+
+store.dispatch(
+  actions.bugRemoved(1)
+);
+
+console.log(store.getState());
